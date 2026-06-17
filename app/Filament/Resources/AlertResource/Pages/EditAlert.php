c631d30ae791a5217f3d2ab9f0c8cfc1b7c4cd1d
Beacon <y.mamdouh@beacon.com.eg>
2026-06-17 12:58:56 +0300

    Add EditAlert page for alert record editing in Filament resource

---
+++ app/Filament/Resources/AlertResource/Pages/EditAlert.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\AlertResource\Pages;
+
+use App\Filament\Resources\AlertResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditAlert extends EditRecord
+{
+    protected static string $resource = AlertResource::class;
+}
