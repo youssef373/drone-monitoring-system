631bd52e92121cf50005bfc8858af484c2df91f7
Beacon <y.mamdouh@beacon.com.eg>
2026-06-19 18:42:12 +0300

    Add map routes for fleet index and single drone views with MapController bindings

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\MapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/map', [MapController::class, 'index'])->name('map.index');
+Route::get('/map/{drone}', [MapController::class, 'show'])->name('map.show');
